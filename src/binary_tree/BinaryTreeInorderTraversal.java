76b8edb90be74d9fbddfe4da9057dcf19f3b54f3
chenwenhang <cwh736@qq.com>
2020-01-21 19:36:44 +0800

    feat: add BinaryTreeInorderTraversal

---
+++ src/binary_tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,50 @@
+package binary_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Wenhang Chen
+ * @Description:给定一个二叉树，返回它的中序 遍历。 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [1,3,2]
+ * @Date: Created in 19:25 1/21/2020
+ * @Modified by:
+ */
+public class BinaryTreeInorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+
+}
